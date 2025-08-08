f26912a0bbae801c529c84c971832bfd48e4d5a5
Mily <mily.sierra@udea.edu.co>
2025-08-08 00:29:07 -0500

    arreglp

--- src/main/java/com/talentotech/prisma/backend/controllers/PreferenciasController.java
+++ src/main/java/com/talentotech/prisma/backend/controllers/PreferenciasController.java
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.talentotech.prisma.backend.dto.PreferenciasDTO;
-import com.talentotech.prisma.backend.services.PreferenciaService;
-
+import com.talentotech.prisma.backend.services.PreferenciasService;
 import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,22 +20,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class PreferenciasController {
 
     @Autowired
-    private PreferenciaService preferenciaService;
+    private PreferenciasService preferenciasService;
 
     @PostMapping
     public ResponseEntity<PreferenciasDTO> ingresarPreferencias(@Valid @RequestBody PreferenciasDTO preferenciasDTO) {
-        PreferenciasDTO guardada = preferenciaService.save(preferenciasDTO);
+        PreferenciasDTO guardada = preferenciasService.save(preferenciasDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(guardada);
     }
 
     @GetMapping("/{id_usuario}/preferencias")
     public ResponseEntity<List<PreferenciasDTO>> getPreferenciasByUsuario(@PathVariable long id) {
-        return ResponseEntity.ok(preferenciaService.findPreferenciasByUsuario(id));
+        return ResponseEntity.ok(preferenciasService.findPreferenciasByUsuario(id));
     }
 
     @GetMapping
     public ResponseEntity<List<PreferenciasDTO>> obtenerPreferencias() {
-        List<PreferenciasDTO> preferencias = preferenciaService.findAll();
+        List<PreferenciasDTO> preferencias = preferenciasService.findAll();
         return ResponseEntity.ok(preferencias);
     }
     
